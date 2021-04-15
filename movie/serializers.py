2e6f48fb6f625178b65d50970911ca8d6bd00fbd
swati som <43273624+swatisom1612@users.noreply.github.com>
2021-04-15 22:37:16 +0530

    Update serializers.py

--- movie/serializers.py
+++ movie/serializers.py
@@ -9,17 +9,17 @@ class MovieCollectionSerializer(serializers.Serializer):
     description = serializers.CharField(required=True, allow_blank=True, allow_null=True)
     movies = serializers.ListField(required=True)
 
-
     @transaction.atomic()
     def create(self, validated_data):
         # getting context of the user
         user = self.context.get('user')
         # creating new movie collection
         collection = MovieCollection.objects.create(user=user, title=validated_data['title'],
-                                                        description=validated_data['description'])
+                                                    description=validated_data['description'])
 
         collection_movies = [
-            MovieGenre(collection=collection, movie_uuid=movie['uuid'], title=movie['title'], description=movie['description'], genres=movie['genres']) for movie in validated_data['movies']
+            MovieGenre(collection=collection, movie_uuid=movie['uuid'], title=movie['title'],
+                       description=movie['description'], genres=movie['genres']) for movie in validated_data['movies']
         ]
 
         # collection created in DB
@@ -27,14 +27,15 @@ class MovieCollectionSerializer(serializers.Serializer):
 
         return collection
 
-
     @transaction.atomic()
     def update(self, instance, validated_data):
         if len(validated_data['movies']) > 0:
             # updating movie list if the payload is not empty
             MovieGenre.objects.filter(collection=instance).delete()
             collection_movies = [
-                MovieGenre(collection=instance, movie_uuid=movie['uuid'], title=movie['title'], description=movie['description'], genres=movie['genres']) for movie in validated_data['movies']
+                MovieGenre(collection=instance, movie_uuid=movie['uuid'], title=movie['title'],
+                           description=movie['description'], genres=movie['genres']) for movie in
+                validated_data['movies']
             ]
 
             MovieGenre.objects.bulk_create(collection_movies)
@@ -45,11 +46,11 @@ class MovieCollectionSerializer(serializers.Serializer):
 
         return instance
 
-
     def to_representation(self, instance):
         def get_movies():
             # get method for movie collection
-            return [movie for movie in instance.collection_movies.values('title', 'description', 'genres', 'movie_uuid')]
+            return [movie for movie in
+                    instance.collection_movies.values('title', 'description', 'genres', 'movie_uuid')]
 
         return {
             'title': instance.title,
